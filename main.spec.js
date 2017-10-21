e6e716037b3b5028346d70ed5c883f6a6040c8b6
Prashant Singh Pawar <prashantpawar@gmail.com>
2017-10-20 20:50:18 -0400

    - Added basic setup for testing infrastructure - Made some changes in main.js to make it better suited for testing

---
+++ main.spec.js
@@ -0,0 +1,31 @@
+describe('eztz.node', () => {
+  let node;
+  let main;
+  let eztz;
+
+  beforeEach(() => {
+    main = require('./main');
+    node = main.eztz.node;
+  });
+
+  test('should have activeProvider set to defaultProvider', () => {
+    expect(node.activeProvider).toBe(main.defaultProvider);
+  });
+
+  test('should provide setProvider method to be able to configure defaultProvider', () => {
+    const previousProvider = node.activeProvider;
+    const newProvider = 'http://example.com/rpc';
+    node.setProvider(newProvider);
+    expect(node.activeProvider).toBe(newProvider);
+    expect(node.activeProvider).not.toBe(previousProvider);
+  });
+
+  test('should provide resetProvider method', () => {
+    const defaultProvider = node.activeProvider;
+    const newProvider = 'http://example.com/rpc';
+    node.setProvider(newProvider);
+    expect(node.activeProvider).toBe(newProvider);
+    node.resetProvider();
+    expect(node.activeProvider).not.toBe(defaultProvider);
+  });
+});
