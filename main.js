078667c41c9a3f1b8659803e33eaee872117ad99
Stephen Andrews <stephen@andrewstribe.co.nz>
2017-10-31 23:12:54 +1300

    performance fixes, contract object, bug fixes and tweaks

--- main.js
+++ main.js
@@ -1,3 +1,43 @@
+//TODO - move functions somewhere else
+function _splitPair(p){
+  var ret = [];
+  if (typeof p.Pair == "undefined"){
+    ret.push(_process(p));
+  } else {
+    ret.push(_process(p.Pair[0][0]));
+    var ss = _splitPair(p.Pair[0][1]);
+    ret = ret.concat(ss);
+  }
+  return ret;
+}
+function _process(p){
+  if (typeof p.Pair != "undefined"){
+    return _splitPair(p);
+  } else if (typeof p.Map != "undefined"){
+    var map = [];
+    var tl = p.Map[0];
+    for(var i = 0; i < tl.length; i++){
+      map.push({
+        key : _process(p.Map[0][i].Item[0][0]),
+        value : _process(p.Map[0][i].Item[0][1])
+      });
+    }
+    return map;
+  } else if (typeof p.List != "undefined"){
+    var list = [];
+    var tl = p.List[0];
+    for(var i = 0; i < tl.length; i++){
+      list.push(_process(p.List[0][i]));
+    }
+    return list;
+  } else if (typeof p.string != "undefined"){
+    return p.string;
+  } else if (typeof p.int != "undefined"){
+    return p.int;
+  } else {
+    return p;
+  }
+}
 const Buffer = require('buffer/').Buffer,
 defaultProvider = "https://tezrpc.me/api",
 library = {
@@ -44,7 +84,57 @@ utility = {
     var hex = '';
     while(length--) hex += chars[(Math.random() * 16) | 0];
     return hex;
-  }
+  },
+  ml2tzjson : function me (mi){
+    if (mi.charAt(0) == "(") mi = mi.slice(1,-1);
+    var pl = 0;
+    var isString = false;
+    var sopen = false;
+    var escaped = false;
+    var ret = [];
+    var val = "";
+    for(var i = 0; i < mi.length; i++){
+      if (escaped){
+        val += mi[i];
+        escaped = false;
+        continue;
+      }
+      else if (i == (mi.length - 1) || (mi[i] == " " && pl == 0 && sopen == false)){
+        if (i == (mi.length - 1)) val += mi[i];
+        if (val){
+          if (val === parseInt(val).toString()) {
+            val = {"int" : val};
+          } else if (ret.length > 0) val = me(val);
+          ret.push(val);
+          val = '';
+        }
+        continue;
+      }
+      else if (mi[i] == '"' && sopen) {
+        sopen = false;  
+        ret.push({'string':val});
+        val = '';
+        continue;
+      }
+      else if (mi[i] == '"' && !sopen) {
+        sopen = true;
+        continue;
+      }
+      else if (mi[i] == '\\') escaped = true;
+      else if (mi[i] == "(") pl++;  
+      else if (mi[i] == ")") pl--;
+      val += mi[i];
+    }
+    if (ret.length > 1){
+      var cc = ret.shift();
+      var oo = {};
+      oo[cc] = [ret, {}]; 
+      return oo;
+    } else {
+      return ret[0];
+    }
+  },
+  tzjson2arr : function(p){return _splitPair(p)}
 },
 crypto = {
   generateMnemonic : function(){
@@ -122,6 +212,7 @@ crypto = {
 }
 node = {
   activeProvider: defaultProvider,
+  async: true,
   setProvider : function(u){
     node.activeProvider = u;
   },
@@ -132,7 +223,7 @@ node = {
     if (typeof o == 'undefined') o = {};
     return new Promise(function (resolve, reject) {
       var http = new XMLHttpRequest();
-      http.open("POST", node.activeProvider + e, true);
+      http.open("POST", node.activeProvider + e, node.async);
       http.onload = function() {
           if(http.status == 200) {
              if (http.responseText){
@@ -166,17 +257,13 @@ rpc = {
   },
   sendOperation : function(operation, keys, fee){
     var head, counter, pred_block, sopbytes;
-    return node.query('/blocks/head')
-    .then(function(f){ 
-      head = f;
-      return node.query('/blocks/prevalidation/proto/context/contracts/'+keys.pkh+'/counter');
-    })
-    .then(function(f){
-      counter = f+1;
-      return node.query('/blocks/prevalidation/predecessor');
-    })
-    .then(function(f){ 
-      pred_block = f.predecessor;
+    var promises = []
+    promises.push(node.query('/blocks/head'));
+    promises.push(node.query('/blocks/prevalidation/proto/context/contracts/'+keys.pkh+'/counter'));
+    return Promise.all(promises).then(function(f){
+      head = f[0];
+      counter = f[1]+1;
+      pred_block = head.predecessor;
       return node.query('/blocks/prevalidation/proto/helpers/forge/operations', {
           "net_id": head.net_id,
           "branch": pred_block,
@@ -206,8 +293,43 @@ rpc = {
     });
   },
 },
-contract = {//TODO
-  
+contract = {
+  originate : function(keys, amount, code, init){
+    //TODO
+  },
+  storage : function(contract){
+    return new Promise(function (resolve, reject) {
+      eztz.node.query("/blocks/head/proto/context/contracts/"+contract).then(function(r){
+        resolve(r.script.storage.storage);
+      }).catch(function(e){
+        reject(e);
+      });
+    });
+  },
+  load : function(contract){
+    return eztz.node.query("/blocks/head/proto/context/contracts/"+contract);
+  },
+  watch : function(contract, timeout, cb){
+    var storage = [];
+    return setInterval(function(){
+      eztz.node.query("/blocks/head/proto/context/contracts/"+contract).then(function(r){
+        var ns = eztz.utility.tzjson2arr(r.script.storage.storage);
+        if (JSON.stringify(storage) != JSON.stringify(ns)){
+          storage = ns;
+          console.log("Found new watch", storage);
+          cb(storage);
+        }
+      });
+    }, timeout*1000);
+  },
+  send : function(contract, keys, amount, parameter, fee){
+    return eztz.rpc.sendOperation({
+      "kind": "transaction",
+      "amount": amount*100,
+      "destination": contract,
+      "parameters": eztz.utility.ml2tzjson(parameter)
+    }, keys, fee);
+  }
 };
 //Expose library
 window.eztz = {
@@ -228,10 +350,7 @@ window.eztz.alphanet.faucet = function(toAddress){
   return node.query('/blocks/head')
   .then(function(f){
     head = f;
-    return node.query('/blocks/prevalidation/predecessor');
-  })
-  .then(function(f){
-    pred_block = f.predecessor;
+    pred_block = head.predecessor;
     return node.query('/blocks/prevalidation/proto/helpers/forge/operations', {
         "net_id": head.net_id,
         "branch": pred_block,
