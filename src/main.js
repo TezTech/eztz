788475f3e5a9534157256d52c2aa5ec555ae9e8b
opspch <35507203+opspch@users.noreply.github.com>
2018-03-06 19:30:56 +0100

    move formatMoney from tezbox to eztz.utility

--- src/main.js
+++ src/main.js
@@ -176,7 +176,16 @@ utility = {
             val += mi[i];
         }
         return ret;
-    }
+  },
+  formatMoney: function(n, c, d, t) {
+    var c = isNaN(c = Math.abs(c)) ? 2 : c, 
+    d = d == undefined ? "." : d, 
+    t = t == undefined ? "," : t, 
+    s = n < 0 ? "-" : "", 
+    i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))), 
+    j = (j = i.length) > 3 ? j % 3 : 0;
+    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+  }
 },
 crypto = {
   generateMnemonic : function(){
