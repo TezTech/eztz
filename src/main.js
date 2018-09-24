9f61af37fcc89c4057f04448b5cec5bda7fcd487
Stephen Andrews <stephen@andrewstribe.co.nz>
2018-09-24 16:59:51 +1200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
-if (typeof require == "undefined") require = require("buffer/").Buffer;
-if (typeof XMLHttpRequest == "undefined") XMLHttpRequest = require('xhr2');;
-  
-
+if (typeof Buffer == "undefined") Buffer = require("buffer/").Buffer;
+if (typeof XMLHttpRequest == "undefined") XMLHttpRequest = require('xhr2');
 const 
 //CLI below
 defaultProvider = "https://rpc.tezrpc.me/",
