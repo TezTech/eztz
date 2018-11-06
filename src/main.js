262b19535439ebfbe5eed7ab13dee0d86679e9c8
Stephen Andrews <stephen@andrewstribe.co.nz>
2018-11-06 13:35:21 +1300

    Silent inject + trezor helper

--- src/main.js
+++ src/main.js
@@ -565,6 +565,13 @@ rpc = {
       };
     });
   },
+	silentInject: function(sopbytes){
+    return node.query('/injection/operation', sopbytes).then(function (f) {
+      return {
+        hash : f
+      };
+    });
+  },
   transfer: function (from, keys, to, amount, fee, parameter, gasLimit, storageLimit) {
     if (typeof gasLimit == 'undefined') gasLimit = '200';
     if (typeof storageLimit == 'undefined') storageLimit = '0';
@@ -718,6 +725,79 @@ contract = {
     return setInterval(ct, timeout * 1000);
   },
 };
+trezor = {
+	source : function(address){
+		var tag = (address[0] == "t" ? 0 : 1);
+		var curve = (parseInt(address[2])-1);
+		var pp = (tag == 1 ? prefix.KT : prefix["tz"+(curve+1)]);
+		var bytes = utility.b58cdecode(address, pp);
+		if (tag == 1) {
+			bytes = utility.mergebuf(bytes, [0])
+		} else {					
+			bytes = utility.mergebuf([curve], bytes)
+		}
+		return {
+			tag : tag,
+			hash : bytes
+		};
+	},
+	operation : function(d){
+		var operations = [];
+		var revealOp = false;
+		var op;
+		for(var i = 0; i < d.opOb.contents.length; i++){
+			op = d.opOb.contents[i];
+			if (op.kind == "reveal"){
+				if (revealOp) throw "Can't have 2 reveals";
+				revealOp = {
+					source : trezor.source(op.source),
+					fee : parseInt(op.fee),
+					counter : parseInt(op.counter),
+					gasLimit : parseInt(op.gas_limit),
+					storageLimit : parseInt(op.storage_limit),
+					publicKey : utility.mergebuf([0], utility.b58cdecode(op.public_key, prefix.edpk)),
+				};
+			} else {
+				if (['origination', 'transaction', 'delegation'].indexOf(op.kind) < 0) return console.log("err2");
+				op2 = {
+					type : op.kind,
+					source : trezor.source(op.source),
+					fee : parseInt(op.fee),
+					counter : parseInt(op.counter),
+					gasLimit : parseInt(op.gas_limit),
+					storageLimit : parseInt(op.storage_limit),
+				};
+				switch(op.kind){
+					case "transaction":
+						op2.amount = parseInt(op.amount);
+						op2.destination = trezor.source(op.destination);
+						if (d.opbytes.length > 172) op2.parameters = utility.hex2buf(d.opbytes.substr(172));
+					break;
+					case "origination":
+						op2.managerPubkey = trezor.source(op.managerPubkey).hash;
+						op2.balance = parseInt(op.balance);
+						op2.spendable = op.spendable;
+						op2.delegatable = op.delegatable;
+						if (typeof op.delegate != 'undefined'){
+							op2.delegate = trezor.source(op.delegate).hash;
+						}
+						//Script not supported yet...
+					break;
+					case "delegation":
+						if (typeof op.delegate != 'undefined'){
+							op2.delegate = trezor.source(op.delegate).hash;
+						}
+					break;
+				}
+				operations.push(op2);
+			}
+		}
+		if (operations.length > 1) return console.log("Too many operations");
+		var operation = operations[0];
+		var tx = {};
+		return [operation, revealOp];
+	}
+};
 
 //Legacy
 utility.ml2tzjson = utility.sexp2mic;
@@ -737,6 +817,7 @@ eztz = {
   node: node,
   rpc: rpc,
   contract: contract,
+  trezor: trezor,
 };
 
 module.exports = {
